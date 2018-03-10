6af07cd825cfdfee7dc8ce93be10f650c55ef845
Vladimir Stankovic <vladast.dev@gmail.com>
2018-03-10 20:55:13 +0100

    Add processing of command line argument
    Added processing of command line options
    for Help and Port.
    
    TG-11 #closed
    
    Change-Id: I992ae3f0067d81de4b291f778424bf3221f14a87

--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
+#include <stdlib.h>
 #include <iostream>
+#include <getopt.h>
 
-int main()
+/**
+ * @brief Prints help menu
+ */
+void print_help();
+
+/**
+ * @brief Main entry-point
+ * @return Program execution status code
+ */
+int main(int argc, char **argv)
 {
-    std::cout << "Test..." << std::endl;
-    return 0;
+    int commandLineOption = 0;
+    uint portNumber = 8089;
+    while ((commandLineOption = getopt(argc, argv, "hp:")) != -1)
+    {
+        switch (commandLineOption) {
+        case 'p':
+            portNumber = atoi(optarg);
+            break;
+        case 'h':
+        default:
+            print_help();
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    std::cout << "Port number: " << portNumber << std::endl;
+
+    return EXIT_SUCCESS;
+}
+
+void print_help()
+{
+    std::string help = "bucketmediator version 0.1 Copyright (c) 2018 Vladimir Stankovic\n"
+                       "usage: bucketmediator [options]\n"
+                       "Options:\n"
+                       "-h              show help\n"
+                       "-p <value>      set port number\n";
+    std::cout << help << std::endl;
 }
