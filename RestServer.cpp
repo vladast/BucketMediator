5ce46515a57d092e818cb5d83cb61a26a61de207
Vladimir Stankovic <vladast.dev@gmail.com>
2018-03-15 23:55:18 +0100

    Run each new erquest on new thread
    Create new thread for each new request.
    Allowed max of 30 client requests.
    
    TG-27
    
    Change-Id: I777f460338dda0ff5de958558e5f89688c408705

--- RestServer.cpp
+++ RestServer.cpp
@@ -5,6 +5,34 @@
 #include <RestRequestProcessor.h>
 #include <sstream>
 #include <vector>
+#include <pthread.h>
+#include <fcntl.h>
+#include <memory>
+
+void* task(void* fd)
+{
+    int clientSocket = *((int*)fd);
+    std::cout << "Processing: " << clientSocket << std::endl;
+    char buffer[1024];
+    int requestHeader = read(clientSocket, buffer, 1024 - 1);
+    std::cout << "Read: " << requestHeader << std::endl;
+
+    buffer[requestHeader] = '\0';
+    std::stringstream ss(buffer);
+    HttpRequest httpRequest(ss);
+    std::cout << "Method: " << httpRequest.GetMethod() << std::endl;
+    std::cout << "URI: " << httpRequest.GetRequestUri() << std::endl;
+
+    RestRequestParams restParams(httpRequest.GetRequestUri());
+
+    RestRequestProcessor restRequestProcessor;
+    HttpResponse httpResponse = restRequestProcessor(restParams);
+
+    std::string response = httpResponse.ToString();
+    write(clientSocket, response.c_str(), response.size());
+
+    close(clientSocket);
+}
 
 RestServer::RestServer(int port, int numberOfPendingConnections) : socketFileDescriptor(-1)
 {
@@ -12,6 +40,10 @@ RestServer::RestServer(int port, int numberOfPendingConnections) : socketFileDes
     this->numberOfPendingConnections = numberOfPendingConnections;
     if (InitServer())
     {
+        int opt = 1;
+        // Set master socket for multiple connections
+        setsockopt(socketFileDescriptor, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt));
+
         socketAddress.sin_family = AF_INET;
         socketAddress.sin_port = htons(port);
         socketAddress.sin_addr.s_addr = INADDR_ANY;
@@ -23,6 +55,12 @@ RestServer::RestServer(int port, int numberOfPendingConnections) : socketFileDes
         {
             socketFileDescriptor = -1; // Indicates an error
         }
+        /*
+        else
+        {
+            fcntl(socketFileDescriptor, F_SETFL, O_NONBLOCK);
+        }
+        */
     }
     running = false;
 }
@@ -45,33 +83,48 @@ void RestServer::Run()
     }
     else
     {
+
+        int threadCount = 0;
+        std::vector<pthread_t> workers;
         running = true;
+        std::cout << "Server socket: " << socketFileDescriptor << std::endl;
+
+        struct sockaddr_storage socketStorage;
+        socklen_t socketStorageSize = sizeof socketStorage;
+
         while (running)
         {
-            struct sockaddr_storage socketStorage;
-            socklen_t socketStorageSize = sizeof socketStorage;
             int clientSocket = accept(socketFileDescriptor,
                                       (struct sockaddr*)&socketStorage,
                                       &socketStorageSize);
-            char buffer[1024];
-            int requestHeader = read(clientSocket, buffer, 1024 - 1);
 
-            buffer[requestHeader] = '\0';
-            std::stringstream ss(buffer);
-            HttpRequest httpRequest(ss);
-            std::cout << "Method: " << httpRequest.GetMethod() << std::endl;
-            std::cout << "URI: " << httpRequest.GetRequestUri() << std::endl;
+            std::cout << "Accepted connection: " << clientSocket << std::endl;
 
-            RestRequestParams restParams(httpRequest.GetRequestUri());
+            pthread_t* worker = new pthread_t;
 
-            RestRequestProcessor restRequestProcessor;
-            HttpResponse httpResponse = restRequestProcessor(restParams);
-
-            std::string response = httpResponse.ToString();
-            write(clientSocket, response.c_str(), response.size());
-
-            close(clientSocket);
+            if (pthread_create(worker, NULL, &task, (void*)&clientSocket) != 0)
+            {
+                std::cout << "Haven't been able to create thread!" << std::endl;
+            }
+            else
+            {
+                workers.push_back(*worker);
+                if (++threadCount > 20 ) // Max of 20 threads allowed
+                {
+                    std::cout << "Breaking out..." << std::endl;
+                    break;
+                }
+            }
         }
+
+        std::cout << "Workers count: " << workers.size() << std::endl;
+        for (auto& worker : workers)
+        {
+            pthread_join(worker, nullptr);
+            std::cout << "Joined" << std::endl;
+        }
+
+        std::cout << "Closing server's socket" << std::endl;
         close(socketFileDescriptor);
     }
 }
